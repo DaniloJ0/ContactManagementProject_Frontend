4e279214479734a3614a438efd3c0f3166cdd75b
Danilo Jimenez <rangeljd@uninorte.edu.co>
2024-04-11 20:05:58 -0500

    Init :tada:

---
+++ contact_management_frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; 
+import './css/main.css'
+import UsersPage from "./pages/UsersPage";
+import ContactsPage from "./pages/ContactsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<UsersPage />} />
+          <Route path="/contacts" element={<ContactsPage/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
